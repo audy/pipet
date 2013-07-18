3db8b0f6985cf5cc59cdcf5e2c4e4f38fa23745d
audy <harekrishna@gmail.com>
2013-07-18 16:11:24 -0400

    require development gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,10 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require 'rspec'
+require 'bundler'
 require 'pipet'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Bundler.require :development
 
 RSpec.configure do |config|
 
