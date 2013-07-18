40f71afa3024db020cf01ecd423d0c2053ef52ff
audy <harekrishna@gmail.com>
2013-07-18 16:38:39 -0400

    happy with database class now

--- lib/database.rb
+++ lib/database.rb
@@ -7,34 +7,34 @@
 
 class Database
 
-
   class << self
-
-    attr_accessor :my_url
 
     ##
     # Specify the URL of a database
     #
-    def url(url)
-      @my_url = url
+    def url(url=nil)
+      @url ||= url
     end
 
     ##
     # Specify MD5 checksum of database
     #
-    def md5
+    def md5(val=nil)
+      @md5 ||= val
     end
 
     ##
     #  Provide a description for the database
     #
-    def description
+    def description(val=nil)
+      @description ||= val
     end
 
     ##
     # Name the database. This is used be `pipet pull`
     #
-    def name
+    def name(val=nil)
+      @name ||= val
     end
 
     ##
@@ -47,8 +47,26 @@ class Database
     # type :structure
     # for a database of protein structures
     #
-    def type
+    def type(val=nil)
+      @type ||= val
     end
 
-  end
+    ##
+    # Make sure all required attributes have been defined.
+    #
+    def validate
+      required_attributes = [
+        :name,
+        :md5,
+        :description,
+        :type
+      ]
+
+      required_attributes.map do |ra|
+        self.send(ra)
+      end.include? nil
+
+    end
+
+  end # class << self
 end
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -2,17 +2,21 @@ require 'spec_helper'
 
 describe Database do
 
-  let :testdb do
-
+  let :test_db do
     class TestDB < Database
-      url 'ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz'
+      url 'hello world!'
+      name 'hello world!'
+      md5 '6f5902ac237024bdd0c176cb93063dc4'
     end
-
-    return TestDB
+    TestDB
   end
 
   it 'should provide a DSL for defining bioinformatics databases' do
-    testdb.my_url.should_not be_nil
+    test_db.should_not be_nil
+  end
+
+  it '#validate should validate attributes' do
+    test_db.validate.should_not be_false
   end
 
 end
