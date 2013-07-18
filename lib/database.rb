9108e0aaebdbf288b8d9de92d45d25dae95cf8ed
audy <harekrishna@gmail.com>
2013-07-18 16:46:48 -0400

    Database.all now returns all database objects

--- lib/database.rb
+++ lib/database.rb
@@ -1,72 +1,12 @@
-##
-# Database
-#
-# Creates a DSL for defining Databases
-# which can be downloaded using `pipet pull`
-#
-
 class Database
-
   class << self
 
     ##
-    # Specify the URL of a database
+    # Returns a list of all objects that inherited from Database
     #
-    def url(url=nil)
-      @url ||= url
+    def all
+      ObjectSpace.each_object(Class).select { |klass| klass < self }
     end
 
-    ##
-    # Specify MD5 checksum of database
-    #
-    def md5(val=nil)
-      @md5 ||= val
-    end
-
-    ##
-    #  Provide a description for the database
-    #
-    def description(val=nil)
-      @description ||= val
-    end
-
-    ##
-    # Name the database. This is used be `pipet pull`
-    #
-    def name(val=nil)
-      @name ||= val
-    end
-
-    ##
-    # Define type of database. This is like a tag.
-    # For example,
-    #
-    # type :nucleotide
-    # for a database of nucleotide sequences
-    #
-    # type :structure
-    # for a database of protein structures
-    #
-    def type(val=nil)
-      @type ||= val
-    end
-
-    ##
-    # Make sure all required attributes have been defined.
-    #
-    def validate
-      required_attributes = [
-        :name,
-        :md5,
-        :description,
-        :type
-      ]
-
-      required_attributes.map do |ra|
-        self.send(ra)
-      end.include? nil
-
-    end
-
-  end # class << self
+  end
 end
