b00fb9be2d83f71bb80f7578bbf870671b6a7b5c
audy <harekrishna@gmail.com>
2013-07-18 17:06:51 -0400

    require Sources and Database

--- lib/pipet.rb
+++ lib/pipet.rb
@@ -1,10 +1,15 @@
 require 'bundler'
 Bundler.require
 
-require 'database'
-require 'database_dsl'
+%w{database database_dsl}.each do |f|
+  require File.join(File.dirname(__FILE__), "#{f}.rb")
+end
+
+
+SOURCES = Dir.glob(File.join(File.dirname(__FILE__), '..', 'Sources', '*.rb'))
+
+SOURCES.each do |sf|
+  require sf
+end
 
 Dir.glob(File.join(File.dirname(__FILE__), 'pipet', '*.rb')).each { |f| require f }
-
-module Pipet
-end
