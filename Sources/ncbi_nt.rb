1b02dbb9eac79b6c1789bc028cc6632680730732
Austin Davis-Richardson <harekrishna@gmail.com>
2015-11-18 13:21:26 -0800

    whitespace

--- Sources/ncbi_nt.rb
+++ Sources/ncbi_nt.rb
@@ -1,8 +1,7 @@
 class NCBINT < Database
-
   name 'ncbi/nt'
+
   url 'ftp://ftp.ncbi.nih.gov/blast/db/FASTA/nt.gz'
   description 'NCBI non-redundant nucleotide database'
   md5 'f77aff452dd1ba6b0e26b8d6b97b0a62'
-
 end
