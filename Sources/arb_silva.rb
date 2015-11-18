f8ae4f1ae6b6f7bdd7d82ca3784f9247e2dd6e23
Austin Davis-Richardson <harekrishna@gmail.com>
2015-11-18 13:22:03 -0800

    + arb/silva

---
+++ Sources/arb_silva.rb
@@ -0,0 +1,7 @@
+class ArbSilva < Database
+  name 'arb/silva'
+
+  url 'http://www.arb-silva.de/fileadmin/silva_databases/current/Exports/SILVA_123_SSURef_Nr99_tax_silva_trunc.fasta.gz'
+  description 'Arb Silav 99% non-redundant unaligned 16S rRNA'
+  md5 'd5deaf307b443194b66bd08a1ee05a66'
+end
